4ebb4593e0430626090864d0ed6fdea625520936
Taylor <tayjoconn@gmail.com>
2021-09-05 10:09:40 -0700

    Cypress test for header
    fixed links in nav, added header tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,18 @@ import logo from '../../assets/logo.png';
 
 function Header() {
   return (
-    <nav>
-      <HashLink to="/">
-        <img className="logo" src={logo} alt="the reddit timer logo" />
-      </HashLink>
-      <div className="NavBar">
-        <HashLink title="Click here to search" to="/search" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>Search</HashLink>
-        <HashLink title="Click here to learn how this app works" to="/#howWork" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>How it works</HashLink>
-        <HashLink title="Click here to learn more about this app" to="/#about" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>About</HashLink>
-      </div>
-    </nav>
+    <heading>
+      <nav>
+        <HashLink to="/">
+          <img className="logo" src={logo} alt="the reddit timer logo" />
+        </HashLink>
+        <div className="NavBar">
+          <HashLink title="Click here to search" to="/search/javascript" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>Search</HashLink>
+          <HashLink title="Click here to learn how this app works" to="#how-it-works" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>How it works</HashLink>
+          <HashLink title="Click here to learn more about this app" to="#about" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>About</HashLink>
+        </div>
+      </nav>
+    </heading>
   );
 }
 
