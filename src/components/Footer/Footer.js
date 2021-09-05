de9427b8f8e43ac30f0e4ffd8065b1b935c65d4f
Taylor <tayjoconn@gmail.com>
2021-09-05 10:24:40 -0700

    cypress test - footer
    Still trying to get exact link to work. https://profy.dev/employers

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import logoSmall from '../../assets/logo_small.png';
 function Footer() {
   return (
     <footer className="footer__wrapper">
-      <a href="https://profy.dev/employers" alt="visit profy.dev/employers">Profy.dev</a>
+      <a href="https://profy.dev/employers" alt="profy.dev/employers">profy.dev</a>
       <img className="logo_small" src={logoSmall} alt="the icon from the reddit search logo" />
       <a href="/terms" alt="Click here to read the terms and privacy conditions">Terms & Privacy</a>
     </footer>
